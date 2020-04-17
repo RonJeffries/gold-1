23c96e1e540e70932b7859aae6770f35ecd9cc19
Ronald Jeffries <ronjeffries@acm.org>
2020-04-17 11:12:47 -0400

    improved master

--- texttest-fixture.rb
+++ texttest-fixture.rb
@@ -24,7 +24,7 @@ Sulfuras, Hand of Ragnaros, -1, 80
 Backstage passes to a TAFKAL80ETC concert, 14, 21
 Backstage passes to a TAFKAL80ETC concert, 9, 50
 Backstage passes to a TAFKAL80ETC concert, 4, 50
-Conjured Mana Cake, 2, 5
+Conjured Mana Cake, 2, 4
 
 "
 
