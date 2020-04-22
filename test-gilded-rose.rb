05fcb2f8ad88ef077407bbbb51289b8b6d728032
Ronald Jeffries <ronjeffries@acm.org>
2020-04-22 09:53:18 -0400

    remove hookup tests

--- test-gilded-rose.rb
+++ test-gilded-rose.rb
@@ -2,16 +2,6 @@ require "./gilded-rose"
 require "minitest/autorun"
 
 class TestGildedRose<Minitest::Test
-  
-  def test_hookup
-    assert true
-  end
-
-  def test_foo
-    items = [Item.new("foo", 0, 0)]
-    GildedRose.new(items).update_quality()
-    assert_equal "foo", items[0].name
-  end
 
   def test_conjured
     gr = GildedRose.new([])
